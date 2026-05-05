092dbbec3a39be01f9871dff6b915beb42253936
parimalapreti <parimalapreti@gmail.com>
2026-05-05 08:51:54 +0530

    Add files via upload

---
+++ stack using an array.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define MAX 5
+int stack[MAX],top=-1;
+void push(int x){
+if(top==MAX-1)
+    printf("stack overflow\n");
+    else{
+        stack[++top]=x;
+        printf("inserted:%d\n",x);
+    }
+}
+void pop()
+{
+
+    if(top==-1)
+        printf("Stack Underflow\n");
+else
+printf("Deleted: %d\n", stack[top--]);
+}
+void display() {
+if(top == -1)
+printf("Stack is Empty\n");
+else {
+for(int i = top; i >= 0; i--)
+printf("%d ", stack[i]);
+printf("\n");
+}
+}
+int main() {
+push(10); push(20); push(30);
+display();
+pop();
+display();
+return 0;
+}
+
